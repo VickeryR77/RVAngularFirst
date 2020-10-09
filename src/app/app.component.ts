4730bfe735e95f4dcb5ede503ff96ed741ed7b4d
Rick Vickery <rickvickery@quickenloans.com>
2020-10-09 00:32:17 -0400

    Added lots of CSS, fixed formatting, added remove feature.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 
-  title = 'Task List';
+  title = 'The Task Master';
   question = "";
+  show: boolean = true;
 
   tasks: ToDo[] = [
-    {taskName: "Walk the Dog", completed: true},
-    {taskName: "Walk the Cat", completed: false},
-    {taskName: "Walk the Bird", completed: true},
-    {taskName: "Have lunch", completed: true},
-    {taskName: "Have dinner", completed: false},
-    {taskName: "Be good at design", completed: false},
-    {taskName: "Prefer front-end", completed: false},
-    {taskName: "Learning Angular", completed: true},
-    {taskName: "Jam Session", completed: true},
-    {taskName: "Another Jam Session", completed: false}
+    {taskName: "Walk the Dog", completed: false},
+    {taskName: "Walk the Cat", completed: true},
+    {taskName: "Walk the Bird", completed: false},
+    {taskName: "Have lunch", completed: false},
+    {taskName: "Have dinner", completed: true},
+    {taskName: "Be good at design", completed: true},
+    {taskName: "Prefer front-end", completed: true},
+    {taskName: "Learning Angular", completed: false},
+    {taskName: "Jam Session", completed: false},
+    {taskName: "Another Jam Session", completed: true}
   ];
+  
 
   changeStatus = function(task: ToDo){
     task.completed=false;
   }
 
   addTask = function(){
-
      this.tasks.push({taskName:`${this.question}`,completed:true});
+  }
+
+  removeTask = function(task: ToDo){
+    this.tasks.splice(this.tasks.indexOf(task),1);
   }
 }
 
